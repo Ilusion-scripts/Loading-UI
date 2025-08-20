16ecce912a040b0e2489d41921b22a332555905a
Miguel7172 <romaozin1607@gmail.com>
2025-08-19 21:54:54 -0300

    Adicionando script6.lua automaticamente

---
+++ script6.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+ID="37e45289-d289-4f2d-af9f-a15ad9bee919";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()```
